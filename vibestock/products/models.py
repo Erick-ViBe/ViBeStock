0b043cb33f74a8c04b7ff9f15432d2fcf7f64c0e
Erick-ViBe <sergio.erick.vicencio.benitez@gmail.com>
2024-04-07 12:40:42 -0600

    feat: product model

--- vibestock/products/models.py
+++ vibestock/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from vibestock.utils.models.base import BaseModel
+
+
+class Product(BaseModel):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    stock = models.IntegerField()
+    expiration_date = models.DateField()
+
+    def __str__(self):
+        return self.name
