8b6c345edcd5011a961ae02c10231b600fe2baf4
Erick-ViBe <sergio.erick.vicencio.benitez@gmail.com>
2024-04-07 12:10:24 -0600

    feat: postgresql database config

--- config/settings.py
+++ config/settings.py
@@ -89,8 +89,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vibestock',
+        'HOST': 'localhost',
     }
 }
 
