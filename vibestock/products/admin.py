9a3f455b847a6815370ce80b4a9288e06a53bde7
Erick-ViBe <sergio.erick.vicencio.benitez@gmail.com>
2024-04-10 06:23:02 -0600

    feat: admin register product models

--- vibestock/products/admin.py
+++ vibestock/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from vibestock.products.models import Product, ExpirationAlert
+
+
+admin.site.register(Product)
+admin.site.register(ExpirationAlert)
